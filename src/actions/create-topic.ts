7ca8200e8c055153b86b82313695e2c2a318be81
Si Thu Win <sthuwin@myzeller.com>
2024-05-09 19:24:04 +1000

    added CREATE topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,78 @@
 "use server";
 
-export async function createTopic() {
-  return { message: "createTopic" };
+import { revalidatePath } from "next/cache";
+import { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import paths from "@/paths";
+import { db } from "@/db";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .max(255)
+    .regex(/^[a-zA-Z0-9_-]/, "Invalid character in name"),
+  description: z.string().min(10).max(255),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be logged in to create a topic"],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["An unknown error occurred"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.home());
+  redirect(paths.topicShow(topic.slug));
 }
