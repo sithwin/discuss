abd478a0a20814a9e238efaf42b366b2ce908274
Si Thu Win <sthuwin@myzeller.com>
2024-05-13 20:01:12 +1000

    post create

---
+++ src/components/posts/post-create-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import { FormButton } from "@/components/common/form-button";
+
+export default function PostCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+          </div>
+
+          <Input
+            name="title"
+            label="Title"
+            labelPlacement="outside"
+            placeholder="title"
+          />
+          <Input
+            name="content"
+            label="Content"
+            labelPlacement="outside"
+            placeholder="Content"
+          />
+
+          <FormButton>Create Post</FormButton>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
